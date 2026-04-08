7cd39a852542c12913e723bfee11b4353fdae6cd
Ubuntu <ubuntu@ip-172-31-24-38.us-west-2.compute.internal>
2026-04-08 17:20:26 +0000

    Moved app.py into app folder for testing purpose

--- app.py
+++ app.py
@@ -1,2 +1,3 @@
 print("Hello, Dosto Day 44 testing caching dependencies in workflows")
 I am testing my smart-trigger workflow which run app changes
+Testing my smart ass trigger workflow which trigger on app changes
